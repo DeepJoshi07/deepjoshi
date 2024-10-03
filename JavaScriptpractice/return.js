85f99a5edba41b6b04756297e29f5a5587ade963
Deep joshi <joshideep972@gmail.com>
2024-10-03 09:05:33 +0530

    js changes

--- JavaScriptpractice/return.js
+++ JavaScriptpractice/return.js
@@ -3,3 +3,17 @@ function sum(a,b){
     return;
     
 }
+let arrow = () =>{
+    console.log("hello world");
+    return;
+};
+let arrow2 = (a,b) =>(a+b);
+let arrow3 = (a,b) => {a*b};
+
+try{
+    if(action === "do it"){
+        console.log("do it");
+    }
+}catch{
+    console.log("there is no action variable");
+}
