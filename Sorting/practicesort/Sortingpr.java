38d41692a4c096bfbac5657fc1b87ca5a01cb0e3
Deep joshi <joshideep972@gmail.com>
2024-10-03 10:42:02 +0530

    Sorting add heap

--- Sorting/practicesort/Sortingpr.java
+++ Sorting/practicesort/Sortingpr.java
@@ -123,6 +123,41 @@ public class Sortingpr {
         }
     }
 
+    private static void heapify(int arr[],int idx,int size){
+        int left = 2*idx+1;
+        int right = 2*idx+2;
+        int maxidx = idx;
+
+        if(left < size && arr[left] > arr[maxidx]){
+            maxidx = left;
+        }
+
+        if(right < size && arr[right] > arr[maxidx]){
+            maxidx = right;
+        }
+
+        if(maxidx != idx){
+            int temp = arr[idx];
+            arr[idx] = arr[maxidx];
+            arr[maxidx] = temp;
+
+            heapify(arr,maxidx,size);
+        }
+    }
+
+    public static void heapsort(int arr[],int idx,int size){
+        for(int i = size/2; i >= 0;i++){
+            heapify(arr,idx,size);
+        }
+        for(int i = size-1;i >= 0;i--){
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr,0,i);
+        }
+    }
+
     public static void printarr(int[]arr){
         for(int i = 0;i < arr.length;i++){
             System.out.print(arr[i] + " ");
@@ -136,7 +171,8 @@ public class Sortingpr {
         // bubblesort(arr);
         // selectionsort(arr);
         // insertionsort(arr);
-        countsort(arr);
+        // countsort(arr);
+        heapsort(arr, 0, arr.length);
         printarr(arr);
     }
 }
