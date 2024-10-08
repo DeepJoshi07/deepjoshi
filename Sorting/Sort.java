5762bd5fd8b9013a48ffe57f9c726c4b3873c4e9
Deep joshi <joshideep972@gmail.com>
2024-10-08 12:06:14 +0530

    add quetions

--- Sorting/Sort.java
+++ Sorting/Sort.java
@@ -33,9 +33,44 @@ public class Sort {
         arr[piv] = temp;
         return i;
     }
+    public static void heapify(int arr[],int idx,int size){
+        int largest = idx;
+        int left = 2*idx+1;
+        int right = 2*idx+2;
+
+        if(left < size && arr[left] >= arr[largest]){
+            largest = left;
+        }
+        if(right < size && arr[right] >= arr[largest]){
+            largest = right;
+        }
+
+        if(largest != idx){
+            int temp = arr[idx];
+            arr[idx] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, largest, size);
+        }
+    }
+    public static void heapsort(int arr[]){
+        int n = arr.length;
+        for(int i = n/2;i >= 0;i--){
+            heapify(arr,i,n);
+        }
+
+        for(int i = n-1;i >= 0;i--){
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+
+            heapify(arr,0,i);
+        }
+    }
     public static void main(String[] args) {
         int []arr ={25,10,5,35,30,20};
-        quicksort(arr,0,arr.length-1);
+        // quicksort(arr,0,arr.length-1);
+        heapsort(arr);
         printlist(arr);
     }
 }
